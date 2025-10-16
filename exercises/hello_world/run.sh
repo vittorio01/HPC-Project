46c7d55726935dd0d33188706a68a9e4df6afc3b
vittorio01 <vittoriopolci@live.com>
2025-10-16 16:14:24 +0200

    deleted:    hello_world/log/err.err 	deleted:    hello_world/log/out.out 	deleted:    hello_world/out/hello_world \	deleted:    hello_world/run.sh

--- exercises/hello_world/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#PBS -l select=1:ncpus=4:mem=2gb
-#PBS -l walltime=0:05:00
-#PBS -q short_cpuQ
-#PBS -e log/err.err
-#PBS -o log/out.out
-cd ${PBS_O_WORKDIR}
-module load mpich-3.2
-mpirun.actual -n 4 out/hello_world
