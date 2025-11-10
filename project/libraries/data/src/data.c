32aec68aca986173fea53dd4ad5f837569c50686
vskywalker01 <vittoriopolci@live.com>
2025-11-10 13:17:53 +0100

    modified:   project/libraries/data/src/data.c 	modified:   project/libraries/data/src/data.h

--- project/libraries/data/src/data.c
+++ project/libraries/data/src/data.c
@@ -1,2 +1,36 @@
 #include "data.h"
 
+void initVector(unsigned int d, Vector* vector) {
+    vector=malloc(sizeof(Vector));
+    vector->d=d;
+    vector->data=malloc(sizeof(float)*d);
+}
+void initVectorRandom(unsigned int d, Vector* vector);
+
+void destroyVector(Vector* vector) {
+    free(vector->data);
+    free(vector);
+}
+
+
+void initMatrix(unsigned int dx, unsigned int dy, Matrix* matrix) {
+    matrix=malloc(sizeof(Matrix));
+    matrix->dx=dx;
+    matrix->dy=dy;
+    matrix->data=malloc(dx*sizeof(float*));
+    for (unsigned int i=0;i<dy;i++) {
+        matrix->data[i]=malloc(dy*sizeof(float));
+    }
+}
+
+void initMatrixRandom(unsigned int dx, unsigned int dy, Matrix* matrix);
+
+void destroyMatrix(Matrix* matrix) {
+    for (unsigned int i=0;i<matrix->dx;i++) {
+        free(matrix->data[i]);
+    }
+    free(matrix->data);
+    free(matrix);
+}
+
+
--- project/libraries/data/src/data.h
+++ project/libraries/data/src/data.h
@@ -4,7 +4,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+definition of vector structure
+d -> dimension of the vector
+data -> array of data
+*/
+typedef struct Vector {
+    float* data;
+    unsigned int d;
+} Vector;
 
+/*
+definition of matrix structure:
+dx -> number of rows
+dy -> number of elements per row
+data -> array of pointers that contins the rows
+*/
+typedef struct Matrix {
+    float** data;
+    unsigned int dx;
+    unsigned int dy;
+} Matrix;
 
+//initVector() generates a veector structure and assigns it at the given vector pointer. Requires:
+//d -> dimension of the vector to create 
+//vector -> a NULL pointer of a vector structue
+//
+void initVector(unsigned int d, Vector* vector);
+void initVectorRandom(unsigned int d, Vector* vector);
+
+//destrouyVector() deallocates the given vector structurr. Requires:
+//vector -> the pointer of the vector structue to deallocate
+void destroyVector(Vector* vector);
+
+//initMatrix() generates a matrix structure and assigns it at the given matrix pointer. Requires:
+//dx -> number of rows of the matrix
+//dy -> dize of the rows of the matrix
+//matrix -> a NULL pointer of a matrix structue
+//
+
+void initMatrix(unsigned int dx, unsigned int dy, Matrix* matrix);
+void initMatrixRandom(unsigned int dx, unsigned int dy, Matrix* matrix);
+
+//destroy() deallocates a matrix structure. Requires:
+//matrix -> the pointer of the matrix structure to deallocate.
+//
+
+void destroyMatrix(Matrix* matrix);
 
 #endif 
